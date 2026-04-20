117897d2755c343206139570ad91d727c9fceeb3
Briyanssss <briyannicho14@gmail.com>
2026-04-20 19:59:13 +0700

    implement basic React components with JSX and ES6

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// Kita ganti App dengan Exercise
+import Exercise from './Exercise.jsx' 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <Exercise />
+  </React.StrictMode>
 )
