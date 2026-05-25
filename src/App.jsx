44de7919d89cd91384e39c08296e648624c0933a
Briyanssss <briyannicho14@gmail.com>
2026-05-26 00:04:47 +0700

    feat: menyelesaikan fitur navigasi dan react icons modul 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import SignInPage from "./pages/signin";
 import SignUpPage from "./pages/signup"; 
 import "./App.css";
-import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorRoute from "./pages/error";
 import DashboardPage from "./pages/dashboard"; 
+import BalancePage from "./pages/balance";
+
 function App() {
   const myRouter = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <div className="flex justify-center items-center min-h-screen">
-          <Link to="/login" className="p-2 m-5 bg-primary text-white rounded-md">
-            Login
-          </Link>
-          |
-          <Link to="/register" className="p-2 m-5 bg-primary text-white rounded-md">
-            Register
-          </Link>
-        </div>
-      ),
-      errorElement: <ErrorRoute />, // Penempatan yang benar ada di dalam objek root ini
+      element: <DashboardPage />,
+      errorElement: <ErrorRoute />, 
     },
     {
       path: "/login",
@@ -29,11 +21,10 @@ function App() {
       path: "/register",
       element: <SignUpPage />,
     },
-    // Karena kamu sudah meng-import Dashboard, mari kita tambahkan rutenya sekalian (Materi 10)
     {
-      path: "/dashboard",
-      element: <DashboardPage />
-    }
+      path: "/balance",
+      element: <BalancePage />,
+    },
   ]);
 
   return (
